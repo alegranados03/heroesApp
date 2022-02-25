6b1eaa1a582eb5b001499196def9e532f5365bd8
Alejandro Granados <alegranados03@gmail.com>
2022-02-25 11:12:18 -0600

    imagen pipe

--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Heroe } from '../interfaces/heroes.interface';
 
 @Pipe({
-  name: 'imagen'
+  name: 'imagen',
 })
 export class ImagenPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(heroe: Heroe): string {
+    if (heroe) {
+      return `assets/heroes/${heroe.id}.jpg`;
+    }
+    return '';
   }
-
 }
